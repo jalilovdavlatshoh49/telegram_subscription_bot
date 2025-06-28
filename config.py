90f6aee0864a75be787406bf665094b4a428c01d
jalilovdavlatshoh49 <143313280+jalilovdavlatshoh49@users.noreply.github.com>
2025-06-28 17:09:31 +0500

    Add/update config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+BOT_TOKEN = "ВАШ_ТОКЕН_БОТА"
+CHANNEL_ID = -1001234567890  # ID приватного канала
+CHANNEL_LINK = "https://t.me/your_channel"
+PAYMENT_LINK = "https://вашбанк.сом/оплата?сумма=5"
+SUBSCRIPTION_DAYS = 30
+GRACE_PERIOD_DAYS = 3
+ADMIN_ID = 123456789  # ваш Telegram ID
